e4877c91bc338207795f5e8b618c9540a80f15ae
ayushanand03 <145312360+ayushanand03@users.noreply.github.com>
2023-09-25 11:03:34 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -35,7 +35,11 @@ st.write(df.describe())
 print("\nMissing Values:")
 print(df.isnull().sum())
 
-
+# Pie chart of summary statistics
+st.subheader('Summary Statistics (BMI Status)')
+status_counts = df['Status'].value_counts()
+fig = px.pie(status_counts, names=status_counts.index, title='BMI Status Distribution')
+st.plotly_chart(fig)
 
 # Sidebar for BMI calculation
 st.sidebar.header('BMI Calculator')
