a67b27feb895a3272d208f8e8114000f9becda8b
ayushanand03 <145312360+ayushanand03@users.noreply.github.com>
2023-09-25 08:45:08 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -28,6 +28,36 @@ if st.button('Calculate BMI'):
         st.write('You are overweight.')
     else:
         st.write('You are obese.')
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Function to calculate BMI
+def calculate_bmi(weight_kg, height_m):
+    return weight_kg / (height_m ** 2)
+
+# Define a range of heights and weights
+heights_m = np.arange(1.5, 2.1, 0.05)  # Heights from 1.5m to 2.0m in 5cm increments
+weights_kg = np.arange(50, 151, 5)  # Weights from 50kg to 150kg in 5kg increments
+
+# Calculate BMI values for all combinations of height and weight
+bmi_values = np.empty((len(heights_m), len(weights_kg)))
+for i, height in enumerate(heights_m):
+    for j, weight in enumerate(weights_kg):
+        bmi = calculate_bmi(weight, height)
+        bmi_values[i, j] = bmi
+
+# Create a heatmap to visualize BMI values
+plt.figure(figsize=(12, 8))
+plt.imshow(bmi_values, cmap='YlGnBu', extent=[50, 150, 1.5, 2.0])
+plt.colorbar(label='BMI')
+plt.xlabel('Weight (kg)')
+plt.ylabel('Height (m)')
+plt.title('BMI Chart')
+plt.xticks(np.arange(50, 151, 10))
+plt.yticks(np.arange(1.5, 2.1, 0.1))
+plt.grid(False)
+plt.show()
+
         
 
  
